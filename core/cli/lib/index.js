908c443b62bb3c7cd3a820badcc02865e7aa60c0
杜金银 <jinyin.du@weimob.com>
2022-07-07 11:31:35 +0800

    qa

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,8 +1,41 @@
 'use strict';
 
 module.exports = core;
+// const path = require('path');
+// const semver = require('semver');
+// const colors = require('colors/safe');
+// const userHome = require('user-home');
+// const pathExists = require('path-exists').sync;
+// const commander = require('commander');
 
-function core() {
-    console.log('exec core')
-    console.log('我是第一次玩脚手架')
+const pkg = require('../package.json');
+
+async function core() {
+    try {
+      await prepare();
+      registerCommand();
+    } catch (e) {
+      log.error(e.message);
+      if (program.debug) {
+        console.log(e);
+      }
+    }
+}
+
+
+function registerCommand() {
+
+}
+
+async function prepare() {
+    checkPkgVersion();
+    // checkRoot();
+    // checkUserHome();
+    // checkEnv();
+    // await checkGlobalUpdate();
+}
+
+// 检查版本号
+function checkPkgVersion(){
+    console.log(pkg.version)
 }
